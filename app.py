980ae2f3b358d9d93d4a4cbd27e860c8a069bdbb
Yoshinori Teraoka <teraoka.yoshinori@gmail.com>
2018-03-18 00:05:12 +0900

    add healthcheck

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from redis import Redis
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
@@ -10,6 +10,11 @@ redis = Redis(host='redis', port=6379)
 def hello():
     count = redis.incr('hits')
     return 'Hello World! I have been seen {} times.\nversion: {}'.format(count, VERSION)
+
+@app.route('/healthcheck')
+def healthcheck():
+    redis.ping()
+    return 'OK'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
