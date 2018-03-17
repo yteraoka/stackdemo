f197a636d51c1080fe0dd855df4f22c4cfa062fe
Yoshinori Teraoka <teraoka.yoshinori@gmail.com>
2018-03-17 23:59:44 +0900

    add version

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from redis import Redis
 
+VERSION = '0.0.1'
+
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
     count = redis.incr('hits')
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\nversion: {}'.format(count, VERSION)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
